fb6e0b2782d2f982426e937fd581d77873bc3140
Almagribii <brucadalm@gmail.com>
2026-07-13 20:16:10 +0700

    fix theme

--- app/src/main/java/com/almagribii/goalmate/ui/theme/Color.kt
+++ app/src/main/java/com/almagribii/goalmate/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.almagribii.goalmate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val IndigoPrimary = Color(0xFF6366F1)
+val SkyBlueSecondary = Color(0xFF38BDF8)
+val OrangeAccent = Color(0xFFF59E0B)
+val AppBackground = Color(0xFFF8FAFC)
+val AppSurface = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Warna untuk Dark Mode
+val DarkBackground = Color(0xFF0F172A)
+val DarkSurface = Color(0xFF1E293B)
